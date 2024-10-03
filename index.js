d70bf08fb21b219f27dca790eb1fa7cf762e00ff
Akshaypareek01 <akshay96102@gmail.com>
2024-10-03 11:11:25 +0530

    working

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.get('/',(req,res)=>{
+  res.send('Welcome to my API')
+})
 app.use('/api/audio', audioRoutes);
 app.use('/api/video', videoRoutes);
 app.use('/api/myData', myDataRoutes);
