098b46f55b605c48307c13153f9efc8cf2f83153
Abhishekanjana <abhishekanjana85@gmail.com>
2024-10-11 16:54:48 +0530

    working

--- routes/audioRecordingRoutes.js
+++ routes/audioRecordingRoutes.js
@@ -2,10 +2,26 @@
 import express from 'express';
 import upload from '../config/upload.js'; // Import the multer upload middleware
 import { uploadAudio } from '../controllers/audioRecordingController.js'; // Import the controller
+import path from 'path';
+import fs from 'fs';
 
 const router = express.Router();
 
 // POST endpoint for uploading audio files
 router.post('/upload', upload.single('audioFile'), uploadAudio);
+router.get('/get/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const filePath = path.join(__dirname, '../uploads', filename);
+  
+    // Check if the file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        return res.status(404).json({ message: 'File not found' });
+      }
+      
+      // Send the audio file as a response
+      res.sendFile(filePath);
+    });
+  });
 
 export default router;
